3411950f434b19a078c58c40ecfc535ffaa4d9e7
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2025-07-23 12:04:33 -0400

    Working (but untested) convolution between random field and tf

--- flake.nix
+++ flake.nix
@@ -2,34 +2,35 @@
   description = "tidal_cosmozoom flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+	nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+	flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config = {
-            allowUnfree = true;
-          };
-        };
-        customFftw_single = pkgs.fftw.override {
-          precision = "single";
-        };
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            gcc
+	flake-utils.lib.eachDefaultSystem (system:
+	  let
+		pkgs = import nixpkgs {
+		  inherit system;
+		  config = {
+			allowUnfree = true;
+		  };
+		};
+		customFftw_single = pkgs.fftw.override {
+		  precision = "single";
+		};
+	  in
+	  {
+		devShell = pkgs.mkShell {
+		  buildInputs = with pkgs; [
+			gcc
 			fftw
+			vtk
 			customFftw_single
-          ];
-        };
-        shellHook = ''
-            echo "Welcome to the tidal_cosmozoom development shell!"
-            '';
+		  ];
+		};
+		shellHook = ''
+			echo "Welcome to the tidal_cosmozoom development shell!"
+			'';
 	  }
-    );
+	);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,6 @@
 #include "fft.hh"
 #include "cosmology.hh"
 
-using FFTW = FFTWTraits<REAL>;
-
 void populate_array_with_random_values(REAL* rand_array, int n, 
 									   std::uniform_real_distribution<> uniform_dist,
 									   std::mt19937 rand_gen,
@@ -30,6 +28,19 @@ void populate_array_with_random_values(REAL* rand_array, int n,
 	}
 }
 
+void print_complex_array(FFTW::complex_type* array, int n0, int n1, int n2) {
+    for (int i = 0; i < n0; ++i) {
+        for (int j = 0; j < n1; ++j) {
+            for (int k = 0; k < n2; ++k) {
+                std::cout << "(" << array[i * n1 * n2 + j * n2 + k][0] << ", "
+                          << array[i * n1 * n2 + j * n2 + k][1] << ") ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
+}
+
 int main(int argc, char *argv[]) {
 	short print_array = 0;
 	const char* filename;
@@ -46,16 +57,36 @@ int main(int argc, char *argv[]) {
 	std::mt19937 rand_gen(params.get<int>("seed"));
 	std::uniform_real_distribution<> uniform_dist(-1.0, 1.0);
 
+	// Allocate arrays
 	int n = params.get<int>("n");
 	REAL *rand_array = new REAL[n*n*n];
 	int fftw_c_size = n * n * (n/2 +1); // Size of half-complex fft
     FFTW::complex_type *rand_array_fft = (FFTW::complex_type*)FFTW::malloc(sizeof(FFTW::complex_type) * fftw_c_size);
+    FFTW::complex_type *tf_array_fft = (FFTW::complex_type*)FFTW::malloc(sizeof(FFTW::complex_type) * fftw_c_size);
     
     populate_array_with_random_values(rand_array, n, uniform_dist, rand_gen, print_array);
     
-    FFTW::plan_type plan;
+	// Get FFT of random noise
+    FFTW::plan_type plan, iplan;
     plan = FFTW::dft_r2c_3d(n, n, n, rand_array, rand_array_fft, FFTW_ESTIMATE);
+    plan = FFTW::dft_c2r_3d(n, n, n, rand_array_fft, rand_array, FFTW_ESTIMATE);
     FFTW::execute(plan);
+
+	// Generate transfer function
+	TransferFunc tf = TransferFunc(params);
+	tf.get_3d_tf_array(tf_array_fft);
+
+	// Convolve white noise with TF in Fourier space 
+	size_t size_n = static_cast<size_t>(n);
+	for (size_t idx = 0; idx < size_n * size_n * (size_n/2 + 1); ++idx) {
+		const REAL re_in = rand_array_fft[idx][0];
+		const REAL im_in = rand_array_fft[idx][1];
+		const REAL Tk = tf_array_fft[idx][0];
+		
+		rand_array_fft[idx][0] = re_in * Tk;
+		rand_array_fft[idx][1] = im_in * Tk;
+	}
+	//print_complex_array(rand_array_fft, n, n, n/2+1);
     
     // Clean up
     FFTW::destroy_plan(plan);
