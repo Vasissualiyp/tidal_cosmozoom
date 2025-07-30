a677d9f59b2a1d7a52137be50f25e01146ea114a
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2025-07-30 18:15:21 -0400

    Abstracted out the field calculation

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,68 @@
 #include "arrays.hh"
 #include "tidal.hh"
 
+void calculate_and_save_fields_from_overdensity(REAL* overdensity, Parameters params) {
+	int n = params.get<int>("n");
+	REAL *grav_potential = new REAL[n*n*n];
+	TensorField<REAL> TidalTensor(n, n, n, true);
+
+	int fftw_c_size = n * n * (n/2 +1); // Size of half-complex fft
+	FFTW::complex_type *grav_potential_fft = (FFTW::complex_type*)FFTW::malloc(sizeof(FFTW::complex_type) * fftw_c_size);
+	FFTW::complex_type *overdensity_fft = (FFTW::complex_type*)FFTW::malloc(sizeof(FFTW::complex_type) * fftw_c_size);
+	TensorField<std::complex<REAL>> TidalTensor_fft(n, n, n/2+1, true);
+
+	FFTW::plan_type plan, iplan_dens, iplan_potential;
+
+	plan = FFTW::dft_r2c_3d(n, n, n, overdensity, overdensity_fft, FFTW_ESTIMATE);
+	FFTW::execute(plan);
+
+	solve_poisson_in_fourier_space(overdensity_fft, grav_potential_fft, params);
+	tidal_tensor_from_potential(grav_potential_fft, TidalTensor_fft, params);
+	ifft_tidal_tensor(TidalTensor_fft, TidalTensor, params);
+
+	// Obtain the gravitational potential
+	iplan_potential = FFTW::dft_c2r_3d(n, n, n, grav_potential_fft, grav_potential, FFTW_ESTIMATE);
+	FFTW::execute(iplan_potential);
+
+	// Get back the overdensity field
+	iplan_dens = FFTW::dft_c2r_3d(n, n, n, overdensity_fft, overdensity, FFTW_ESTIMATE);
+	FFTW::execute(iplan_dens);
+
+	renormalize_post_fft_array(overdensity, n);
+	renormalize_post_fft_array(grav_potential, n);
+
+	write_field_to_binary_file(overdensity,    n, "out/overdensity.bin");
+	write_field_to_binary_file(grav_potential, n, "out/potential.bin");
+	TidalTensor.write_tensor_to_binary_files("");
+
+	FFTW::destroy_plan(plan);
+	FFTW::destroy_plan(iplan_dens);
+	FFTW::free(grav_potential_fft);
+	delete[] grav_potential;
+}
+
+int* get_new_center_from_largest_potential(FFTW::complex_type* overdensity_fft, Parameters params) {
+	int n = params.get<int>("n");
+	REAL *grav_potential = new REAL[n*n*n];
+
+	int fftw_c_size = n * n * (n/2 +1); // Size of half-complex fft
+	FFTW::complex_type *grav_potential_fft = (FFTW::complex_type*)FFTW::malloc(sizeof(FFTW::complex_type) * fftw_c_size);
+	FFTW::plan_type iplan_potential;
+
+	// Obtain the gravitational potential
+	solve_poisson_in_fourier_space(overdensity_fft, grav_potential_fft, params);
+	iplan_potential = FFTW::dft_c2r_3d(n, n, n, grav_potential_fft, grav_potential, FFTW_ESTIMATE);
+	FFTW::execute(iplan_potential);
+
+	// Identify the location of max
+	int* max_loc = find_loc_of_max_in_array(grav_potential, n, false);
+	FFTW::destroy_plan(iplan_potential);
+	FFTW::free(grav_potential_fft);
+	delete[] grav_potential;
+
+	return max_loc;
+}
+
 int main(int argc, char *argv[]) {
 	using namespace std::complex_literals;
 	short print_array = 0;
@@ -30,68 +92,46 @@ int main(int argc, char *argv[]) {
 
 	// Real data arrays
 	int n = params.get<int>("n");
-	REAL *rand_array = new REAL[n*n*n];
-	REAL *grav_potential = new REAL[n*n*n];
-	TensorField<REAL> TidalTensor(n, n, n, true);
 
 	// FFT arrays
 	int fftw_c_size = n * n * (n/2 +1); // Size of half-complex fft
-	FFTW::complex_type *rand_array_fft = (FFTW::complex_type*)FFTW::malloc(sizeof(FFTW::complex_type) * fftw_c_size);
+	FFTW::complex_type *overdensity_fft = (FFTW::complex_type*)FFTW::malloc(sizeof(FFTW::complex_type) * fftw_c_size);
 	FFTW::complex_type *tf_array_fft   = (FFTW::complex_type*)FFTW::malloc(sizeof(FFTW::complex_type) * fftw_c_size);
-	FFTW::complex_type *grav_potential_fft = (FFTW::complex_type*)FFTW::malloc(sizeof(FFTW::complex_type) * fftw_c_size);
-	TensorField<std::complex<REAL>> TidalTensor_fft(n, n, n/2+1, true);
 	
-	populate_array_with_random_values(rand_array, n, uniform_dist, rand_gen, print_array);
+	REAL *overdensity = new REAL[n*n*n];
+	populate_array_with_random_values(overdensity, n, uniform_dist, rand_gen, print_array);
 	
 	// Get FFT of random noise
-	FFTW::plan_type plan, iplan_dens, iplan_potential;
-	plan = FFTW::dft_r2c_3d(n, n, n, rand_array, rand_array_fft, FFTW_ESTIMATE);
+	FFTW::plan_type plan, iplan_dens;
+	plan = FFTW::dft_r2c_3d(n, n, n, overdensity, overdensity_fft, FFTW_ESTIMATE);
 	FFTW::execute(plan);
 
 	// Generate transfer function
 	TransferFunc tf = TransferFunc(params);
 	tf.get_3d_tf_array(tf_array_fft);
-	tf.convolve_array_with_tf(rand_array_fft, tf_array_fft);
-	
-	// Solve for the gravitational potential and tidal fields in the Fourer space
-	solve_poisson_in_fourier_space(rand_array_fft, grav_potential_fft, params);
-	tidal_tensor_from_potential(grav_potential_fft, TidalTensor_fft, params);
-	ifft_tidal_tensor(TidalTensor_fft, TidalTensor, params);
+	tf.convolve_array_with_tf(overdensity_fft, tf_array_fft);
+	FFTW::free(tf_array_fft);
+	int* new_center = get_new_center_from_largest_potential(overdensity_fft, params);
 
-	// Obtain the gravitational potential
-	iplan_potential = FFTW::dft_c2r_3d(n, n, n, grav_potential_fft, grav_potential, FFTW_ESTIMATE);
-	FFTW::execute(iplan_potential);
-
-	// Get back the overdensity field
-	iplan_dens = FFTW::dft_c2r_3d(n, n, n, rand_array_fft, rand_array, FFTW_ESTIMATE);
+	// Get the overdensity field 
+	iplan_dens = FFTW::dft_c2r_3d(n, n, n, overdensity_fft, overdensity, FFTW_ESTIMATE);
 	FFTW::execute(iplan_dens);
+	shift_array_to_pos<REAL>(overdensity, new_center, n);
 
-	renormalize_post_fft_array(rand_array, n);
-	renormalize_post_fft_array(grav_potential, n);
-	// Cutting the array test
-	int dn = 16;
-	int n_cut = n - 2*dn;
-	REAL *rand_array_cut = new REAL[n_cut*n_cut*n_cut];
-	cut_boundaries(rand_array, rand_array_cut, params, dn);
-	//Parameters cut_params = cut_boundaries(rand_array, rand_array_cut, params, dn);
-	//
-	int* max_loc = find_loc_of_max_in_array(grav_potential, n, false);
-	shift_array_to_pos<REAL>(grav_potential, max_loc, n);
-	write_field_to_binary_file(rand_array,     n,     "out/overdensity.bin");
-	write_field_to_binary_file(rand_array_cut, n_cut, "out/overdensity_cut.bin");
-	write_field_to_binary_file(grav_potential, n,     "out/potential.bin");
-	TidalTensor.write_tensor_to_binary_files("");
-	delete[] rand_array_cut; 
-	//print_complex_array(grav_potential_fft, n, n, n/2+1);
-	
-	// Clean up
+	FFTW::free(overdensity_fft);
 	FFTW::destroy_plan(plan);
 	FFTW::destroy_plan(iplan_dens);
-	FFTW::destroy_plan(iplan_potential);
-	FFTW::free(rand_array_fft);
-	FFTW::free(grav_potential_fft);
-	delete[] rand_array; 
-	delete[] grav_potential;
+	
+	calculate_and_save_fields_from_overdensity(overdensity, params);
+	// Cutting the array test
+	//int dn = 16;
+	//int n_cut = n - 2*dn;
+	//REAL *rand_array_cut = new REAL[n_cut*n_cut*n_cut];
+	//cut_boundaries(rand_array, rand_array_cut, params, dn);
+	//Parameters cut_params = cut_boundaries(rand_array, rand_array_cut, params, dn);
+	
+	// Clean up
+	delete[] overdensity; 
 	
 	return 0;
 }
